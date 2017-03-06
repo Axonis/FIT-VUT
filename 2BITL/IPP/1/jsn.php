6f322c05954fa127079c05459994fdc74fd72cb0
Jozef Urbanovský <xurban66@vutbr.cz>
2017-03-06 22:49:20 +0100

    JSN WIP

--- 2BITL/IPP/1/jsn.php
+++ 2BITL/IPP/1/jsn.php
@@ -1,239 +1,262 @@
 #!/usr/bin/php
 <?php
 
-//var_dump($argv);
+$arguments = parser($argv, $argc);
+$input = file_handler($arguments["input"],"r");
+$json = json_validator($input);
+$output = file_handler($arguments["output"],"w");
+write($json, $arguments);
 
-//$test = parser::$input; // pristupovanie k premennych vnutri
-//echo $test;
-
-
-parser::load($argv);
-
-if(parser::$help)
+function file_handler($file, $type)
 {
-    help();
-    die(0);
+    //This function may return Boolean FALSE, but may also return a non-Boolean value which evaluates to FALSE.
+    // Please read the section on Booleans for more information. Use the === operator for testing the return value of this function.
+    if($type == "r") {
+        if (($handle = file_get_contents($file)) === false) {
+            throw new errException("Failed to read input file.", 2);
+        }
+        return $handle;
+    }
+
+    if($type == "w") {
+        if (($handle = fopen($file, $type)) == false) {
+            throw new errException("Failed to write to output file.",3);
+        }
+        else {
+            fclose($handle);
+        }
+        return $handle;
+    }
+    throw new errException("Function file_handler got different parameters than expected.",500);
 }
 
-$handle_input = fopen(parser::$input, "r");
-$handle_output = fopen(parser::$output, "w");
-$input_str = fread($handle_input, filesize(parser::$input));
-$input_str_json = json_decode($input_str,true);
+function json_validator($string)
+{
+    $json = json_decode($string, false);
+    if(!is_object($json) && !is_array($string)){
+        throw new errException("Input file does not have correct JSON annotation.",4);
+    }
+    return $json;
+}
 
-xml::xml_encoder($input_str_json);
-
-
-
-
-
-class parser {
-
-    static
-        // NULL for input
-        // BOOL for options
-        $help = FALSE,
-        $input = "php://stdin",
-        $output = "php://stdout",
-        $substitution = "-",
-        $header = TRUE,
-        $root = NULL,
-        $array = NULL,
-        $item = NULL,
-        $string = FALSE,
-        $number = FALSE,
-        $literal = FALSE,
-        $conversion = FALSE,
-        $size = FALSE,
-        $items = FALSE,
-        $start = 1,
-        $types = FALSE,
-        $dependency = FALSE;
-        // TODO
-
-    public static function load(array $argv){
-        // Unsets first argument which is name of the script
-        unset($argv[0]);
-
-        foreach($argv as $arg){
-
-            if($arg === "--help"){
-                if(self::$help){
-                    throw new errException(1);
-                }
-                self::$help = TRUE;
+function parser($arguments, $count)
+{
+    // Array of arguments with their respective parameters (argument => parameter)
+    $arg_array = array();
+    // Last argument being parsed
+    $parameters = array();
+    // Arg 0 = name of script
+    if (($count == 2) && ($arguments[1] === "--help")) {  // if there is only help defined
+        help();
+    }
+    for ($i = 1; $i < $count; $i++) {
+        if (preg_match("/^--(input|output|array-name|item-name|start|types)=(.*)/", $arguments[$i], $parameters)
+            || preg_match("/^-(r|h)=(.*)/", $arguments[$i], $parameters) //
+        ) {   // array[param[1]] (already in array) = param[2] (new parameter parsed)
+            if (empty($arg_array[$parameters[1]]) && ($parameters[2] !== NULL)) { // argument=parameter already defined
+                $arg_array[$parameters[1]] = $parameters[2];
+            } else { // Argument already set - error
+                throw new errException("Option cannot be set twice.", 1);
             }
-
-            elseif(substr($arg, 0, 8) === "--input="){
-                if(self::$input !== "php://stdin"){
-                    throw new errException(1);
-                }
-                self::$input = substr($arg, 8);
+        } elseif (preg_match("/^-(n|s|i|l|c|a|t)$/", $arguments[$i], $parameters)
+            || preg_match("/^--(index-items|array-size)$/", $arguments[$i], $parameters)
+        ) {   // when regex matches options without value, [1]is option [2]is null
+            if (empty($arg_array[$parameters[1]])) {
+                $arg_array[$parameters[1]] = true;
+            } else {  // Argument already set - error
+                throw new errException("Option cannot be set twice.", 1);
             }
-
-            elseif(substr($arg, 0, 9) === "--output="){
-                if(self::$output !== "php://stdout"){
-                    throw new errException(1);
-                }
-                self::$output = substr($arg, 9);
-            }
-
-            elseif($arg === "-n"){
-                if(!(self::$header)){
-                    throw new errException(1);
-                }
-                self::$header = FALSE;
-            }
-
-            elseif(substr($arg, 0, 3) === "-h="){
-                if(self::$substitution !== "-"){
-                    throw new errException(1);
-                }
-                //TODO CHECK SUBST STRING FOR ERRORS
-                self::$substitution = substr($arg, 3);
-            }
-
-            elseif(substr($arg, 0, 13) === "--array-name="){
-                if(self::$array !== NULL){
-                    throw new errException(1);
-                }
-                self::$array = substr($arg, 13);
-            }
-
-            elseif(substr($arg, 0, 12) === "--item-name="){
-                if(self::$item !== NULL){
-                    throw new errException(1);
-                }
-                self::$item = substr($arg, 12);
-            }
-
-            elseif($arg === "-s"){
-                if(self::$string){
-                    throw new errException(1);
-                }
-                self::$string = TRUE;
-            }
-
-            elseif($arg === "-i"){
-                if(self::$number){
-                    throw new errException(1);
-                }
-                self::$number = TRUE;
-            }
-
-            elseif($arg === "-l"){
-                if(self::$literal){
-                    throw new errException(1);
-                }
-                self::$literal = TRUE;
-            }
-
-            elseif($arg === "-c"){
-                if(self::$conversion){
-                    throw new errException(1);
-                }
-                self::$conversion = TRUE;
-            }
-
-            elseif($arg === "-a" || $arg === "--array-size"){
-                if(self::$size){
-                    throw new errException(1);
-                }
-                self::$size = TRUE;
-            }
-
-            elseif($arg === "-t" || $arg === "--index-items"){
-                if(self::$items){
-                    throw new errException(1);
-                }
-                //TODO START DEPENDENCY
-                self::$items = TRUE;
-            }
-
-            elseif(substr($arg, 0, 8) === "--start="){
-
-                self::$dependency = TRUE;
-
-                if(self::$start !== 1){
-                    throw new errException(1);
-                    //TODO START 1
-                }
-                else{
-                    self::$start = substr($arg, 8);
-                    // Dependency index-items and start
-                    if (!(preg_match('!^[1-9][0-9]*$!', self::$start))) {
-                        throw new errException(1);
-                    }
-                }
-            }
-
-            else{
-                // Unknown argument, unable to process
-                throw new errException(1);
-            }
-
-            // Help single arg. check
-            if(count($argv) > 1 && self::$help) {
-                throw new errException(1);
-            }
-
-            //Start indexing default value check
-            if(self::$items && self::$start === NULL){
-                self::$start = 1;
-            }
-        }
-
-        if(self::$dependency && !(self::$items)){
-            throw new errException(1);
+        } else { // Set argument that is not defined
+            throw new errException("Option not defined.", 1);
         }
     }
 
+    // Options having default value and not being specified
+    if (empty($arg_array["input"])) {
+        $arg_array["input"] = "php://stdin";
+    }
+    if (empty($arg_array["output"])) {
+        $arg_array["output"] = "php://stdout";
+    }
+    if (empty($arg_array["h"])) {
+        $arg_array["h"] = "-";
+    }
+    if (!isset($arg_array["start"])) { // Empty would detect 0 as true
+        $arg_array["start"] = 1;
+    } else {
+        // Check dependency of start and index-items
+        if (empty($arg_array["t"]) && empty($arg_array["index-items"])) {
+            throw new errException("Option \"--start\" has dependency on \"--index-items\" or \"-t\".", 1);
+        } elseif (!ctype_digit($arg_array["start"])) {
+            throw new errException("Option \"--start\" requires unsigned integer value.", 1);
+        } else {   // casting to integer
+            $arg_array["start"] = (int)$arg_array["start"];
+        }
+
+    }
+
+    // Same short and long options set at the same time
+    if (!empty($arg_array["t"]) && !empty($arg_array["index-items"])) {
+        throw new errException("\"-t\" and \"--index-items\" cannot be specified at the same time.", 1);
+    }
+    if (!empty($arg_array["a"]) && !empty($arg_array["array-size"])) {   // when -a is set --array-size can not be set it is the same option
+        throw new errException("\"-a\" and \"--array-size\" cannot be specified at the same time.", 1);
+    }
+
+
+    // TODO CHECK
+    if (empty($arg_array["array-name"])) {
+        $arg_array["array-name"] = "array";
+    } else {   // when array-name is changed script will check name, not valid causes error 50
+        $arg_array["array-name"] = check_name($arg_array["array-name"], $arg_array["h"], false);
+    }
+    if (empty($arg_array["item-name"])) {
+        $arg_array["item-name"] = "item";
+    } else {   // when item-name is changed script will check name, not valid causes error 50
+        $arg_array["item-name"] = check_name($arg_array["item-name"], $arg_array["h"], false);
+    }
+
+
+    return $arg_array;
 }
 
-class xml {
-    private
-        $data = NULL,
-        $output = NULL,
-        $element = NULL,
-        $attribute = NULL;
 
+/*
+** initialization of writer and calling write_ functions
+**/
+function write($json_input, $args)
+{
+    $writer = new XMLWriter();
+    $writer->openURI($args["output"]);
+    $writer->setIndent(true);
+    if (empty($args["n"])) {   // header
+        $writer->startDocument("1.0", "UTF-8");
+    }
+    if (!empty($args["r"])) {   // placing result into "root-element" not valid name causes error 50
+        $writer->startElement(check_name($args["r"], $args["h"], false));
+        write_value($writer, $json_input, $args);
+        $writer->endElement();
+    } else {
+        write_value($writer, $json_input, $args);
+    }
+}
 
-    public function xml_encoder($json){
-        foreach ($json as $data){
-            echo $data;
-            $output = $data."\n";
+/*
+** recursively called for writing objects
+*/
+function write_object($writer, $object, $args)
+{
+    foreach ($object as $key => $value) {   // when string subst is invalid character causes error 51
+        $writer->startElement(check_name($key, $args["h"], true));
+        write_value($writer, $value, $args);
+        $writer->endElement();
+    }
+}
 
+/*
+** recursively called for writing arrays
+*/
+function write_array($writer, $array, $args)
+{
+    $writer->startElement($args["array-name"]);
+    if (!empty($args["array-size"]) || !empty($args["a"])) {
+        $writer->writeAttribute("size", count($array));
+    }
+    $index = $args["start"];    // index counter initialization for every array
+    foreach ($array as $key => $value) {
+        $writer->startElement($args["item-name"]);
+        if (!empty($args["index-items"]) || !empty($args["t"])) {
+            $writer->writeAttribute("index", $index++);
+        }
+        write_value($writer, $value, $args);
+        $writer->endElement();
+    }
+    $writer->endElement();
+}
+
+/*
+** function for writing values
+*/
+function write_value($writer, $value, $args)
+{
+    if (is_object($value)) {
+        write_object($writer, $value, $args);
+    } elseif (is_array($value)) {
+        write_array($writer, $value, $args);
+    } elseif ((is_int($value) || is_float($value))) {
+        $value = floor($value); // need to floor number values before writing
+        if (!empty($args["i"])) {   // when option -i is set
+            $writer->text($value);
+        } else {
+            $writer->writeAttribute("value", $value);
+        }
+    } elseif ((empty($args["s"]) && is_string($value))) {
+        $writer->writeAttribute("value", $value);
+    } elseif (($value === true || $value === false || $value === null)) {   // handling litterals true false and null
+        if (!empty($args["l"])) {   // when -l option is set for replacing literal with <literal/> tag
+            if ($value === true) {
+                $writer->writeElement("true");
+            } elseif ($value === false) {
+                $writer->writeElement("false");
+            } elseif ($value === null) {
+                $writer->writeElement("null");
+            }
+        } else {
+            if ($value === true) {
+                $writer->writeAttribute("value", "true");
+            } elseif ($value === false) {
+                $writer->writeAttribute("value", "false");
+            } elseif ($value === null) {
+                $writer->writeAttribute("value", "null");
+            }
+        }
+    } elseif (!empty($args["c"])) {   // translation of problematic characters
+        $writer->text($value);
+    } else {
+        $writer->writeRaw($value);
+    }
+}
+
+/*
+** function for checking names and replacing invalid characters
+**/
+function check_name($name, $replacement, $allow_replace)
+{
+    $start_char_rex = '/^[^\p{L}|\_]/';
+    $validity_rex = '/<|>|"|\'|\/|\\|&|&/';
+    if (preg_match($start_char_rex, $name) || preg_match($validity_rex, $name)) {   // if regex matches there is invalid character
+        if ($allow_replace) {
+            $name = preg_replace($validity_rex, $replacement, $name);
+            $name = preg_replace($start_char_rex, $replacement, $name);
+            if (preg_match($start_char_rex, $name) || preg_match($validity_rex, $name)) {
+                throw new errException("Invalid name of XML element after substitution.",51);
+            }
+        } else {
+            throw new errException("Invalid XML tag.",50);
         }
     }
+    return $name;
 }
 
 
 // Class for handling errors (errorException is not fitting)
-class errException extends Exception {
+class errException extends Exception
+{
     public $code, $message;
 
-    public function __construct($code){
+    public function __construct($code, $message)
+    {
         $this->code = $code;
+        $this->message = $message;
     }
 
-    public function __toString(){
-        if($this->code == 1){
-            $this->message = "špatný formát parametrů skriptu nebo byla použita zakázaná kombinace parametrů";
-        }
-        elseif($this->code == 2){
-            $this->message = "neexistující zadaný vstupní soubor nebo chyba otevření zadaného vstupního souboru pro čtení";
-        }
-        elseif($this->code == 3){
-            $this->message = "chyba při pokusu o otevření zadaného výstupního souboru pro zápis";
-        }
-        elseif($this->code == 4){
-            $this->message = "chybný formát vstupního souboru";
-        }
-        //TODO
-
-        return "Error: ".$this->message." (error code: ".$this->code.")\n";
+    public function __toString()
+    {
+        return "Error: $this->code (Error Number: $this->message)\n";
     }
 }
 
+// Function to output help
 function help()
 {
     echo " 
@@ -292,6 +315,7 @@ n včetně nuly (implicitně n = 1).
 hodnotu integer pro původní celočíselnou hodnotu, real pro původní desetinnou hodnotu,
 string pro řetězec a literal pro hodnoty literálů.
 ";
+    die(0);
 }
 
 
