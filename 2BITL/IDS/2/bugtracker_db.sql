337d9d5782b5dd8da97f9cce5fc52c137fcdb6fe
Adrian Tomasov <atomasov@redhat.com>
2017-03-24 20:35:28 +0100

    IDS

--- 2BITL/IDS/2/bugtracker_db.sql
+++ 2BITL/IDS/2/bugtracker_db.sql
@@ -14,8 +14,8 @@ SET FOREIGN_KEY_CHECKS=1;
 
 CREATE TABLE Modul (
   ID_modul int NOT NULL,
-  chybovost bigint DEFAULT NULL,
-  datum_poslednej_upravy datetime DEFAULT NULL,
+  chybovost float DEFAULT NULL,
+  datum_poslednej_upravy date DEFAULT NULL,
   PRIMARY KEY (ID_modul)
 );
 
@@ -39,18 +39,50 @@ CREATE TABLE Ticket (
 );
 
 
-CREATE TABLE Programator(
-  ID_employee int NOT NULL,
-  Rank int,
+CREATE TABLE Uzivatel(
+
+  Nickname int NOT NULL,
+  Meno varchar(128) NOT NULLa,
+  Vek int,
+  Programovaci_jazyk varchar(512),
 
   ID_modul int,
   ID_ticket int,
   ID_test int,
+  ID_patch int,
 
 
-
-  PRIMARY KEY (ID_employee),
+  PRIMARY KEY (Nickname),
   FOREIGN KEY (ID_ticket)       REFERENCES Ticket(ID_ticket),
-  FOREIGN KEY (ID_modul)        REFERENCES Modul(ID_modul)
+  FOREIGN KEY (ID_modul)        REFERENCES Modul(ID_modul),
+  FOREIGN KEY (ID_test)		REFERENCES Test(ID_test),
+  FOREIGN KEY (ID_patch)	REFERENCES Patch(ID_patch)
 );
+
+CREATE TABLE Test(
+  ID_test int NOT NULL,
+  Datum_zaciatku datetime,
+  Datum_konca datetime, 
+  Hodnotenie float,
+
+  ID_patch int NOT NULL,
+  ID_uzivatel,
+
+  PRIMARY KEY (ID_test),
+  FOREIGN KEY (ID_patch)	REFERENCES Patch(ID_patch),
+  FOREIGN KEY (ID_uzivatel)	REFERENCES Uzivatel(ID_uzivatel)
+  
+);
+
+CREATE TABLE Patch(
+  ID_patch int NOT NULL,
+  Schvalenie bit,
+  Datum_vydania date,
+  Datum_zavedenia date,
+
+  ID_uzivatel,
+
+  PRIMARY KEY (ID_patch)
+);
+
 
