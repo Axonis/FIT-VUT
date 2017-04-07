d34474353f63ebd08fa51609c45a0a8c9e3b3545
Jozef Urbanovský <xurban66@vutbr.cz>
2017-04-07 23:33:38 +0200

    update

--- 2BITL/IDS/3/xurban66_xtomas32.sql
+++ 2BITL/IDS/3/xurban66_xtomas32.sql
@@ -17,7 +17,7 @@ CREATE TABLE Modul (
   chybovost float DEFAULT NULL,
   datum_poslednej_upravy date DEFAULT NULL,
   
-  Nickname NVARCHAR2(64),
+  Nickname_zodpovedny NVARCHAR2(64),
   
   PRIMARY KEY (ID_modul)
 );
@@ -48,7 +48,7 @@ CREATE TABLE Patch(
   Datum_vydania date,
   Datum_zavedenia date,
 
-  Nickname NVARCHAR2(64),
+  Nickname_vydany NVARCHAR2(64),
 
   PRIMARY KEY (ID_patch)
 );
@@ -60,7 +60,7 @@ CREATE TABLE Test (
   Hodnotenie float,
 
   ID_patch int,
-  Nickname NVARCHAR2(64),
+  Nickname_schvaleny NVARCHAR2(64),
 
   PRIMARY KEY (ID_test)
 );
@@ -114,15 +114,15 @@ CREATE TABLE Zranitelnost(
   PRIMARY KEY (ID_vulnerablity)   
 );
 
-ALTER TABLE Modul ADD FOREIGN KEY (Nickname) REFERENCES Uzivatel;
+ALTER TABLE Modul ADD FOREIGN KEY (Nickname_zodpovedny) REFERENCES Uzivatel;
 ALTER TABLE Bug ADD FOREIGN KEY (ID_modul) REFERENCES Modul;
 ALTER TABLE Bug ADD FOREIGN KEY (ID_patch) REFERENCES Patch;
 ALTER TABLE Ticket ADD FOREIGN KEY (Nickname_prideleny) REFERENCES Uzivatel(Nickname);
 ALTER TABLE Ticket ADD FOREIGN KEY (Nickname_vyvoreny) REFERENCES Uzivatel(Nickname);
 
-ALTER TABLE Patch ADD FOREIGN KEY (Nickname) REFERENCES Uzivatel(Nickname);
+ALTER TABLE Patch ADD FOREIGN KEY (Nickname_vydany) REFERENCES Uzivatel(Nickname);
 ALTER TABLE Test ADD FOREIGN KEY (ID_patch) REFERENCES Patch(ID_patch);
-ALTER TABLE Test ADD FOREIGN KEY (Nickname) REFERENCES Uzivatel(Nickname);
+ALTER TABLE Test ADD FOREIGN KEY (Nickname_schvaleny) REFERENCES Uzivatel(Nickname);
 
 ALTER TABLE Charakterizuje ADD FOREIGN KEY (ID_ticket) REFERENCES Ticket(ID_ticket);
 ALTER TABLE Charakterizuje ADD FOREIGN KEY (ID_bug) REFERENCES Bug(ID_bug);
@@ -152,30 +152,33 @@ INSERT INTO Bezny_uzivatel (Nickname, Odmena, Karma) VALUES('Cyborg13', null, nu
 INSERT INTO Bezny_uzivatel (Nickname, Odmena, Karma) VALUES('Destroyer-ultimate', '10000', '1000');
 INSERT INTO Bezny_uzivatel (Nickname, Odmena, Karma) VALUES('Whysoserious', '56464', '56');
 
-INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname) VALUES('1', '1,25', '21-12-2016', 'NovaZilina');
-INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname) VALUES('2', '2,83', '24-03-2017', 'MirrorMaster12');
-INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname) VALUES('3', '1,14', '17-02-2017', 'xxKebabmajsterxx');
-INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname) VALUES('4', '3,84', '16-11-2015', 'AndreDankojeLegenda');
-INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname) VALUES('5', '0,12', '14-01-2016', 'xxKebabmajsterxx');
+INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname_zodpovedny) VALUES('1', '1,25', '21-12-2016', 'NovaZilina');
+INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname_zodpovedny) VALUES('2', '2,83', '24-03-2017', 'MirrorMaster12');
+INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname_zodpovedny) VALUES('3', '1,14', '17-02-2017', 'xxKebabmajsterxx');
+INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname_zodpovedny) VALUES('4', '3,84', '16-11-2015', 'AndreDankojeLegenda');
+INSERT INTO Modul (ID_modul, chybovost, datum_poslednej_upravy, Nickname_zodpovedny) VALUES('5', '0,12', '14-01-2016', 'xxKebabmajsterxx');
 
-INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia) VALUES('20170217', '0', '03-12-2017', '');
-INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia) VALUES('20170219', '1', '07-11-2017', '05-12-2017');
-INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia) VALUES('20170218', '1', '06-10-2017', '06-12-2017');
-INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia) VALUES('20170216', '1', '05-09-2017', '07-12-2017');
-INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia) VALUES('20170212', '1', '04-08-2017', '08-12-2017');
+INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia, Nickname_vydany) VALUES('20170217', '0', '03-12-2017', '', 'xxKebabmajsterxx');
+INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia, Nickname_vydany) VALUES('20170219', '1', '07-11-2017', '05-12-2017', 'Ahojakosamas');
+INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia, Nickname_vydany) VALUES('20170218', '1', '06-10-2017', '06-12-2017', 'Cyborg13');
+INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia, Nickname_vydany) VALUES('20170216', '1', '05-09-2017', '07-12-2017', 'Knedla8');
+INSERT INTO Patch (ID_patch, Schvalenie, Datum_vydania, Datum_zavedenia, Nickname_vydany) VALUES('20170212', '1', '04-08-2017', '08-12-2017', 'Knedla8');
 
 INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost, ID_patch) VALUES('2190732', '1', 'Logic', 'High', '20170219');
 INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost, ID_patch) VALUES('2190746', '2', 'Syntax', 'Medium', '20170216');
-INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost) VALUES('2190789', '3', 'Not a bug', 'Low');
+INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost, ID_patch) VALUES('2190789', '3', 'Not a bug', 'Low', '20170218');
 INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost) VALUES('2190719', '4', 'Interfacing', 'Medium');
 INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost) VALUES('2190729', '5', 'Resource', 'Low');
+INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost) VALUES('2165646', '1', 'Logic', 'High');
+INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost) VALUES('2132213', '2', 'Syntax', 'Medium');
+INSERT INTO Bug (ID_bug, ID_modul, Typ, Zavaznost) VALUES('6897983', '3', 'Logic', 'High');
 
 INSERT INTO Zranitelnost (ID_vulnerablity, Miera_nebezpecenstva, Moznost_zneuzitia) VALUES('2190732', 'High', 'Modul');
 INSERT INTO Zranitelnost (ID_vulnerablity, Miera_nebezpecenstva, Moznost_zneuzitia) VALUES('2190746', 'Critical', 'System');
 
 INSERT INTO Ticket (ID_ticket, Datum_podania, Stav, Nickname_prideleny, Nickname_vyvoreny) VALUES('46864', '23-03-2017', 'Open', 'xxKebabmajsterxx', 'Cyborg13');
 INSERT INTO Ticket (ID_ticket, Datum_podania, Stav, Nickname_prideleny, Nickname_vyvoreny) VALUES('48864', '13-02-2017', 'Assigned', 'MirrorMaster12', 'Knedla8');
-INSERT INTO Ticket (ID_ticket, Datum_podania, Stav, Nickname_prideleny, Nickname_vyvoreny) VALUES('41864', '23-01-2017', 'New', 'Whysoserious', 'Cyborg13');
+INSERT INTO Ticket (ID_ticket, Datum_podania, Stav, Nickname_prideleny, Nickname_vyvoreny) VALUES('41864', '23-01-2017', 'New', '', 'Cyborg13');
 INSERT INTO Ticket (ID_ticket, Datum_podania, Stav, Nickname_prideleny, Nickname_vyvoreny) VALUES('41865', '02-11-2017', 'Closed', 'Ahojakosamas', 'Knedla8');
 INSERT INTO Ticket (ID_ticket, Datum_podania, Stav, Nickname_prideleny, Nickname_vyvoreny) VALUES('41868', '24-03-2017', 'Need Info', 'Destroyer-ultimate', 'Cyborg13');
 
@@ -183,10 +186,35 @@ INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu
 INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu, Popis_vyskytu, Popis_problemu) VALUES ('48864', '2190732', '01-03-2017', '7', '1.Install System, 2.Run cmd command', 'Kernel Panic');
 INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu, Popis_vyskytu, Popis_problemu) VALUES ('48864', '2190746', '01-03-2017', '10', '1.Install System, 2.Run cmd command', 'Kernel Panic');
 INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu, Popis_vyskytu, Popis_problemu) VALUES ('41864', '2190746', '01-03-2017', '7', '1.Install System, 2.Run cmd command', 'Kernel Panic');
+INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu, Popis_vyskytu, Popis_problemu) VALUES ('46864', '2165646', '01-03-2017', '8', '1.Install System, 2.Run cmd command', 'Kernel Panic');
+INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu, Popis_vyskytu, Popis_problemu) VALUES ('48864', '2132213', '01-03-2017', '9', '1.Install System, 2.Run cmd command', 'Kernel Panic');
+INSERT INTO Charakterizuje (ID_ticket, ID_bug, Datum_vyskytu, Frekvencia_vyskytu, Popis_vyskytu, Popis_problemu) VALUES ('41864', '6897983', '01-03-2017', '1', '1.Install System, 2.Run cmd command', 'Kernel Panic');
 
-INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname) VALUES('20170102', '01-03-2017', '01-03-2017', '98,6', '20170217', 'xxKebabmajsterxx');
-INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname) VALUES('20170214', '01-04-2017', '01-05-2017', '91,8', '20170219', null);
-INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname) VALUES('20170215', '02-03-2017', '05-03-2017', '100,0', '20170218', null);
-INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname) VALUES('20170216', '07-01-2017', '07-05-2017', '100,0', '20170216', null);
-INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname) VALUES('20170217', '13-03-2017', '13-03-2018', '9,6', '20170212', 'Ahojakosamas');
+INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname_schvaleny) VALUES('20170102', '01-03-2017', '01-03-2017', '98,6', '20170217', 'xxKebabmajsterxx');
+INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname_schvaleny) VALUES('20170214', '01-04-2017', '01-05-2017', '91,8', '20170219', null);
+INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname_schvaleny) VALUES('20170215', '02-03-2017', '05-03-2017', '100,0', '20170218', null);
+INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname_schvaleny) VALUES('20170216', '07-01-2017', '07-05-2017', '100,0', '20170216', null);
+INSERT INTO Test (ID_test, Datum_zaciatku, Datum_konca, Hodnotenie, ID_patch, Nickname_schvaleny) VALUES('20170217', '13-03-2017', '13-03-2018', '9,6', '20170212', 'Ahojakosamas');
 
+/*2 JOIN - Mena programatorov s rankom vyssim ako 5, ktori schvalili testy s hodnotenim nizsim ako 90*/
+SELECT Test.Nickname_schvaleny FROM Test JOIN Programator ON Test.Nickname_schvaleny = Programator.Nickname WHERE Test.Hodnotenie <= 90 AND Programator.Rank > 5;
+
+/*2 JOIN + IN - Aka je zavaznost bugov ktore su zatial nepridelene*/
+SELECT Zavaznost FROM BUG WHERE ID_bug IN (SELECT ID_bug FROM Ticket JOIN Charakterizuje ON Ticket.ID_ticket = Charakterizuje.ID_ticket WHERE Stav = 'New');
+
+/*3 JOIN + IN - Na akych bugoch pracuje dany programator*/
+SELECT ID_Bug FROM Bug WHERE ID_bug IN (SELECT ID_bug FROM Ticket JOIN Charakterizuje ON Ticket.ID_ticket = Charakterizuje.ID_ticket WHERE Ticket.Nickname_prideleny = 'xxKebabmajsterxx');
+
+/*2 JOIN - Aky rank ma uzivatel ktory zodpoveda za modul cislo 2*/
+SELECT Rank FROM Programator JOIN Modul ON Programator.Nickname = Modul.Nickname_zodpovedny WHERE ID_modul = 2;
+
+/*3 JOIN - Aky patch opravil zranitelnost bugu 2190732*/
+SELECT Patch.ID_patch FROM Bug JOIN Patch ON Bug.ID_patch = Patch.ID_patch JOIN Zranitelnost ON Zranitelnost.ID_vulnerablity = Bug.ID_bug WHERE ID_vulnerablity = 2190732;
+
+/*3 JOIN + EXISTS - Ktore zranitelnosti sa testuju*/
+SELECT * FROM Zranitelnost WHERE EXISTS (SELECT ID_bug FROM Test JOIN Patch ON Test.ID_patch = Patch.ID_patch JOIN Bug ON Patch.ID_patch = Bug.ID_patch);
+
+/* - Ake hodnotenie ma posledny dokonceny test*/
+SELECT Hodnotenie FROM Test GROUP BY Hodnotenie;
+SELECT AVG(Hodnotenie) FROM Test;
+--SELECT Nickname, COUNT(ID_patch) FROM
